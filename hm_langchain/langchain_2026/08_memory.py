a8819e1828429214bf1343be2473bd6e86ccbf75
zhanghuan <huan.zhang@bafangseis.com>
2026-07-13 11:37:02 +0800

    memory

--- hm_langchain/langchain_2026/08_memory.py
+++ hm_langchain/langchain_2026/08_memory.py
@@ -7,12 +7,24 @@ from langgraph.graph.message import REMOVE_ALL_MESSAGES
 from langgraph.runtime import Runtime
 from dotenv import load_dotenv
 from langgraph.checkpoint.redis import RedisSaver
-from typing import Any
+from langgraph.store.redis import RedisStore
+from typing import Any, NotRequired
+from langchain_community.embeddings import DashScopeEmbeddings
+from langchain.tools import tool, ToolRuntime
+import os
 
 load_dotenv()
 
+redis_url = os.getenv("REDIS_URL")
+dashscope_api_key = os.getenv("DASHSCOPE_API_KEY")
+
 model = init_chat_model(
     model="deepseek:deepseek-v4-flash"
+)
+
+embeddings = DashScopeEmbeddings(
+    model='text-embedding-v3',
+    dashscope_api_key=dashscope_api_key
 )
 
 
@@ -41,14 +53,14 @@ def demo_01():
 
 
 def demo_02():
-    with RedisSaver.from_conn_string("xxx") as checkpointer:
+    with RedisSaver.from_conn_string(redis_url) as checkpointer:
         checkpointer.setup()
         agent = create_agent(
             model=model,
             checkpointer=checkpointer
         )
         config = {
-            "configurable": {"thread_id": "1"}
+            "configurable": {"thread_id": "2"}
         }
         response = agent.invoke({
             "messages": [HumanMessage("我叫张三")]},
@@ -114,5 +126,148 @@ def demo_03():
         msg.pretty_print()
 
 
+def demo_04():
+    with RedisStore.from_conn_string(redis_url) as store:
+        store.setup()
+        namespace1 = ("users", "Alice", "memories")
+        key1 = "preferences"
+        value1 = {
+            "course": "计算机组成原理",
+            "sports": "跑步",
+            "food": "紫光园奶皮子酸奶"
+        }
+        namespace2 = ("users", "Bob", "memories")
+        key2 = 'preferences'
+        value2 = {
+            "course": "数字电路与模拟电路",
+            "sports": "跑步",
+            "food": "奶皮子糖葫芦"
+        }
+        namespace3 = ("users", "Black", "memories")
+        key3 = 'preferences'
+        value3 = {
+            "course": "数字电路与模拟电路",
+            "sports": "羽毛球",
+            "food": "紫光园奶皮子酸奶"
+        }
+
+        store.put(namespace1, key1, value1)
+        store.put(namespace2, key2, value2)
+        store.put(namespace3, key3, value3)
+
+        print("----store finished----")
+
+
+def demo_05():
+    with RedisStore.from_conn_string(redis_url) as store:
+        store.setup()
+        # for item in store.search(("users",)):
+        #     print(item)
+        # for item in store.search(("users", "Alice")):
+        #     print(item)
+        for item in store.search(("users",), filter={"food": "紫光园奶皮子酸奶"}):
+            print(item)
+        print('----search finished----')
+
+
+def demo_06():
+    index_config = {
+        "embed": embeddings,
+        "dims": 1024,
+        "fields": ["$"]
+    }
+    with RedisStore.from_conn_string(redis_url, index=index_config) as store:
+        store.setup()
+        namespace1 = ("users", "Alice", "memories")
+        key1 = "preferences"
+        value1 = {
+            "course": "计算机组成原理",
+            "sports": "跑步",
+            "food": "紫光园奶皮子酸奶"
+        }
+        namespace2 = ("users", "Bob", "memories")
+        key2 = 'preferences'
+        value2 = {
+            "course": "数字电路与模拟电路",
+            "sports": "跑步",
+            "food": "奶皮子糖葫芦"
+        }
+        namespace3 = ("users", "Black", "memories")
+        key3 = 'preferences'
+        value3 = {
+            "course": "数字电路与模拟电路",
+            "sports": "羽毛球",
+            "food": "紫光园奶皮子酸奶"
+        }
+        store.put(namespace1, key1, value1)
+        store.put(namespace2, key2, value2)
+        store.put(namespace3, key3, value3)
+
+
+def demo_07():
+    index_config = {
+        "embed": embeddings,
+        "dims": 1024,
+        "fields": ["$"]
+    }
+    with RedisStore.from_conn_string(redis_url, index=index_config) as store:
+        store.setup()
+        for item in store.search(("users",), query="数电模电"):
+            print(item)
+
+
+class CustomState(AgentState):
+    user_id: NotRequired[str]
+
+
+@tool
+def save_user_info(name: str, runtime: ToolRuntime) -> str:
+    """
+    将用户信息保存在长期记忆中
+    Args:
+        name: 用户名
+    Returns:
+        str: 保存结果
+    """
+    runtime.store.put(("users",), runtime.state["user_id"], {"name": name})
+    return "saved"
+
+
+@tool
+def get_user_info(runtime: ToolRuntime) -> str:
+    """
+    从长期记忆中获取用户信息
+    Returns:
+        str: 用户信息
+    """
+    item = runtime.store.get(("users",), runtime.state["user_id"])
+    return str(item.value) if item else "unknown"
+
+
+def demo_08():
+    with RedisStore.from_conn_string(redis_url) as store:
+        store.setup()
+        agent = create_agent(
+            model=model,
+            tools=[save_user_info, get_user_info],
+            store=store,
+            system_prompt="用户提及个人信息时及时记录，用户询问个人信息时尝试用工具检索",
+            state_schema=CustomState
+        )
+        # res1 = agent.invoke({
+        #     "messages": "很高兴认识你，我是小花",
+        #     "user_id": "user-1"
+        # })
+        # for msg in res1["messages"]:
+        #     msg.pretty_print()
+
+        res2 = agent.invoke({
+            "messages": "你知道我是谁吗？",
+            "user_id": "user-1"
+        })
+        for msg in res2["messages"]:
+            msg.pretty_print()
+
+
 if __name__ == '__main__':
-    demo_03()
+    demo_08()
